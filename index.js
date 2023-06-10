0f8d2c672ccde6d597ea5c1120ef2d8fbaf6b880
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2023-06-10 03:16:10 +0100

    ...

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+
 const socket = require('socket.io');
 
 const app = require('./app');
@@ -13,10 +13,7 @@ const server = app.listen(3030, () => {
 
 const io = socket(server, {
     cors: {
-        origin: (origin, callback) => {
-            callback(null, true);
-        },
-        credentials: true
+        origin: '*'
     }
 });
 
@@ -35,5 +32,6 @@ io.on('connection', socket => {
             socket.to(sendUserSocket).emit('msg-receive', data.message)
         }
     })
+
 
 })
